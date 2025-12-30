b5cd9eafa2850a97c4f40dd9855f1bd69a9d4720
Marco Castellanos <marcopoulut@gmail.com>
2025-12-30 11:55:52 -0800

    adding helper functions

--- config/config.go
+++ config/config.go
@@ -1,12 +1,46 @@
 package config
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
+	"sort"
+	"time"
+	"github.com/mcbk51/scores_dash/api"
 )
 
 
-func fetchScores() []Game {
+func FindNextGame(league string) time.Time {
+	for i := 1 ; i < 7 ; i++ {
+		futureDate := time.Now().AddDate(0, 0, i)
+		games, err := api.GetGames(league, futureDate)
+		if err != nil {
+			continue
+		}
+		if len(games) > 0 {
+			sort.Slice(games, func(i, j int) bool {
+				return games[i].StartTime.Before(games[j].StartTime)
+			})
+			return games[0].StartTime
+		}
+	}
+	return time.Time{}
 }
+
+func IsLive(status string) bool {
+	return status == "STATUS_IN_PROGRESS" || status == "In Progress" || status == "STATUS_HALFTIME" || status == "Halftime"
+}
+
+func IsFinal(status string) bool {
+	return status == "STATUS_FINAL" || status == "Final"
+}
+
+func CountLiveGames(games []api.Game) int {
+	count := 0
+	for _, game := range games {
+		if IsLive(game.Status) {
+			count++
+		}
+	}
+	return count
+}
+
+
+
